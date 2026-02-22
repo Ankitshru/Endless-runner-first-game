05c89e7777d81c78c604ea201be62a83cdaedee8
Ankit shrungare <ankitshrungare80@gmail.com>
2026-02-22 07:17:45 +0530

    Day 2 - Added collision detection and GameOver script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,5 +15,10 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition; 
+
+
+        Debug.Log(player.position);
+        Debug.Log(transform.position);
+
     }
 }
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,55 @@
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public static GameOver Instance;
+
+    [SerializeField] private GameObject gameOverUI;
+
+    private bool isGameOver = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(false);
+        }
+    }
+
+    public void GameOverNow()
+    {
+        if (isGameOver) return;
+
+        isGameOver = true;
+
+        Time.timeScale = 0f;
+
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(true);
+
+        }
+    }
+
+    private void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
